c447ffb14f2a3cde0096502ef1631400519d23e1
jasminumbi <jasmin.umbanhowar@gmail.com>
2024-09-17 17:36:37 -0500

    classes pt 2 studio

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -4,5 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        MenuItem item1 = new MenuItem("Gyoza", "Japanese style pan fried pot sticker", 5.95, "appetizer");
+        MenuItem item2 = new MenuItem("Harumaki", "Japanese spring roll", 4.95, "appetizer");
+        MenuItem item3 = new MenuItem("Shrimp teriyaki bento box", "Served with california roll, vegetable tempura, shumai, crab rangoon, spring roll, white rice, salad and miso soup", 17.95, "main course");
+        MenuItem item4 = new MenuItem("Hamachi tataki", "Ponzu sauce topped with yellowtail and garnished with jalapeno. served with miso soup and house salad", 15.95, "main course");
+        MenuItem item5 = new MenuItem("Cheese cake tempura", "A deep fried cheesecake. Crunchy on the outside and melted sweetness inside", 4.95, "dessert");
+
+        System.out.println(item1);
+
+        Menu menu = new Menu();
+
+        menu.addItem(item1);
+        menu.addItem(item2);
+        menu.addItem(item3);
+        menu.addItem(item4);
+        menu.addItem(item5);
+
+        System.out.println(menu);
+
+        menu.removeItem(item2);
+        System.out.println(menu);
+
+        System.out.println(item1.equals(item5));
     }
 }
--- classes-part-2/studio/src/main/java/org/launchcode/Menu.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Menu.java
@@ -1,31 +1,49 @@
 package org.launchcode;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Menu {
-    private Date lastUpdated;
-    private ArrayList<MenuItem> items;
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
+    public ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private LocalDate lastUpdated;
+
+    public ArrayList<MenuItem> getMenuItems() {
+    return menuItems;
     }
 
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
+    @Override
+    public String toString() {
+        StringBuilder appetizers = new StringBuilder();
+        for (MenuItem item: menuItems) {
+            if (item.getCategory().equals("appetizer")) {
+                appetizers.append("\n").append(item.toString()).append("\n");
+            }
+        } StringBuilder mainCourses = new StringBuilder();
+        for (MenuItem item: menuItems) {
+            if (item.getCategory().equals("main course")) {
+                mainCourses.append("\n").append(item.toString()).append("\n");
+            }
+        } StringBuilder desserts = new StringBuilder();
+        for (MenuItem item: menuItems) {
+            if (item.getCategory().equals("dessert")) {
+                desserts.append("\n").append(item.toString()).append("\n");
+            }
+        }
+        return "\nSUSHI AI MENU\n" +
+                "APPETIZERS" + appetizers.toString() + "\n" +
+                "MAIN COURSES" + mainCourses.toString() + "\n" +
+                "DESSERTS" + desserts.toString() +"\n";
     }
 
-    public void setItems(ArrayList<MenuItem> items) {
-        this.items = items;
+    void addItem(MenuItem newItem) {
+        menuItems.add(newItem);
+        lastUpdated = LocalDate.now();
     }
 
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public ArrayList<MenuItem> getItems() {
-        return items;
+    void removeItem(MenuItem item) {
+        menuItems.remove(item);
+        lastUpdated = LocalDate.now();
     }
 }
 
