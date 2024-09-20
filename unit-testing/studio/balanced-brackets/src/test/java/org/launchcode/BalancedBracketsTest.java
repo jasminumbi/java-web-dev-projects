b1205e4798dded37e3cbb4177b9fad4012c80434
jasminumbi <jasmin.umbanhowar@gmail.com>
2024-09-19 20:19:33 -0500

    so many balanced brackets

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -6,6 +6,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
     //TODO: add tests here
+    String test_word;
+    @BeforeEach
+    public void createTestWord() {
+        test_word = "[Banana]";
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void nullTest() {
+        assertNotNull(test_word);
+    }
+
+    @Test
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void oneBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertnotNull(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertEquals(BalancedBrackets.hasBalancedBrackets("[]", "[]"));
+    }
+
+    @Test
+    public void noBrackets() {
+        String bracketless = "Nope";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(bracketless));
+    }
+
+    @Test
+    public void inTheMiddle() {
+        String test = "Ban[ana";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(test));
+    }
+
+    @Test
+    public void unbalanced() {
+        String unbalancedString = "][";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(unbalancedString));
+    }
+
+    @Test
+    public void unbalancedInString() {
+        String launch = "Launch]Code[";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(launch));
+    }
+
+    @Test
+    public void emptyString() {
+        String empty = "";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(empty));
+    }
+
     @Test
     public void emptyTest() {
         assertEquals(true, true);
