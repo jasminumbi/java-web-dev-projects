f5bfee2c0b28a71c517817c3564a2b72ec336964
jasminumbi <jasmin.umbanhowar@gmail.com>
2024-10-03 19:53:01 -0500

    debugging

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -3,20 +3,32 @@ package org.launchcode;
 public class Main {
     public static void main(String[] args) {
 
-        // TODO: Declare and initialize a CD and a DVD object.
+        //--breakpoint 1--//
+        //--constructor calls w/ required parameters--//
         BaseDisc CD = new CD ("Dream Theater", "music");
         BaseDisc DVD = new DVD("The Fellowship of the Ring", "movie");
-        // TODO: Call each CD and DVD method to verify that they work as expected.
 
+        //--breakpoint 2--//
+        //--behaviors--//
         ((CD) cd).spinDisc();
         cd.storeData();
         cd.writeData();
         ((CD) cd).readData();
 
+        //--breakpoint 3--//
+        //--some additional behaviors--//
+
         ((DVD) dvd).spinDisc();
         dvd.storeData();
         dvd.writeData();
         ((DVD) dvd).spinDisc();
+
+        //--throw error--//
+        try {
+            throw new Error("Bad.");
+        } catch(Error e){
+            e.printStackTrace();
+        }
 
         System.out.println((DVD) dvd).destroyMicrowave());
     }
